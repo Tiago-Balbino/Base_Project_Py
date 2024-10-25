51e5b8ccc7e72dfc5ec268c06dd53bf555869aad
Tiago-Balbino <tiago1haise@gmail.com>
2024-10-25 15:48:38 -0300

    Initialize FastAPI project with Docker setup
    This commit initializes a new FastAPI project, adding essential setup files including `main.py`, `docker-compose.yml`, and a `Dockerfile`. It also includes IntelliJ IDEA configuration files under the `.idea` directory and adds core dependencies in `requirements.txt`. The codebase is set up to handle CORS, and initial HTTP request tests are provided in `test_main.http`.

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+routers = []
+
+for router in routers:
+    app.include_router(router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3002",
+    "http://192.168.0.10:8012",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
